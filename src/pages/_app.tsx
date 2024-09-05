7c01207cf1a395ff109effc6ead86eaf034d7b53
이민선 <woooooooow22@gmail.com>
2024-09-06 00:42:20 +0900

    feat: base url 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import RootLayout from "./RootLayout"
+import axios from "axios"
 import "@/styles/globals.css"
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+	axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
 	return (
 		<RootLayout>
 			<QueryClientProvider client={queryClient}>
