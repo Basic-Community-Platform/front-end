15558bd81aab4e4ab1b4b59ed92a8428bf309bdf
이민선 <woooooooow22@gmail.com>
2024-09-05 23:53:04 +0900

    feat: QueryClientProvider 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import RootLayout from "./RootLayout"
 import "@/styles/globals.css"
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<RootLayout>
-			<Component {...pageProps} />
+			<QueryClientProvider client={queryClient}>
+				<Component {...pageProps} />
+			</QueryClientProvider>
 		</RootLayout>
 	)
 }
