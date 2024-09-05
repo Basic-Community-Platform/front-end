4ff01269b4bead5342726ff6b911f4dab9464337
이민선 <woooooooow22@gmail.com>
2024-09-06 01:46:32 +0900

    feat: 댓글 작성 폼 컴포넌트 생성

---
+++ src/modules/board/components/CommentForm.tsx
@@ -0,0 +1,57 @@
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea"
+
+const formSchema = z.object({
+	comment: z.string().min(1, "댓글을 입력해주세요."),
+})
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+	console.log(values)
+}
+
+type CommentFormProps = {
+	commentCount: number
+}
+
+export const CommentForm = ({ commentCount }: CommentFormProps) => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			comment: "",
+		},
+	})
+	return (
+		<section className="w-screen py-12">
+			<h2 className="p-1">{commentCount}개의 댓글</h2>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<FormField
+						control={form.control}
+						name="comment"
+						render={({ field, fieldState: { error } }) => (
+							<FormItem className="w-2/3">
+								<FormControl>
+									<Textarea placeholder="댓글을 입력하세요" {...field} />
+								</FormControl>
+								{error ? (
+									<FormMessage />
+								) : (
+									<div className="h-[1.25rem]"></div> // 에러 메시지가 없을 때 공간 유지
+								)}
+							</FormItem>
+						)}
+					/>
+					<div className="flex w-2/3 flex-row justify-end gap-x-2">
+						<Button type="submit" className="w-1/7">
+							댓글 작성
+						</Button>
+					</div>
+				</form>
+			</Form>
+		</section>
+	)
+}
