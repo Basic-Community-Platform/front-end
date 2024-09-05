819f80e4c0c4cd3080992b001b0f3e48b4bc9597
이민선 <woooooooow22@gmail.com>
2024-09-05 21:42:21 +0900

    style: 에러메시지 출력 시 input 위치가 올라가는 현상 해결

--- src/modules/auth/components/SignInForm.tsx
+++ src/modules/auth/components/SignInForm.tsx
@@ -29,31 +29,39 @@ export const SignInForm = () => {
 			<h1 className="w-1/2 min-w-[400px] py-4 text-xl">로그인</h1>
 			<form
 				onSubmit={form.handleSubmit(onSubmit)}
-				className="flex h-full w-1/2 min-w-[400px] flex-col items-center justify-center space-y-8 rounded-l border border-slate-200 py-16"
+				className="flex h-full w-1/2 min-w-[400px] flex-col items-center justify-center rounded-l border border-slate-200 py-16"
 			>
 				<FormField
 					control={form.control}
 					name="loginId"
-					render={({ field }) => (
+					render={({ field, fieldState: { error } }) => (
 						<FormItem className="w-2/3">
 							<FormLabel>아이디</FormLabel>
 							<FormControl>
 								<Input placeholder="아이디를 입력하세요" {...field} />
 							</FormControl>
-							<FormMessage />
+							{error ? (
+								<FormMessage />
+							) : (
+								<div className="h-[1.25rem]"></div> // 에러 메시지가 없을 때 공간 유지
+							)}
 						</FormItem>
 					)}
 				/>
 				<FormField
 					control={form.control}
 					name="password"
-					render={({ field }) => (
+					render={({ field, fieldState: { error } }) => (
 						<FormItem className="w-2/3">
 							<FormLabel>비밀번호</FormLabel>
 							<FormControl>
 								<Input placeholder="비밀번호를 입력하세요" {...field} />
 							</FormControl>
-							<FormMessage />
+							{error ? (
+								<FormMessage />
+							) : (
+								<div className="h-[1.25rem]"></div> // 에러 메시지가 없을 때 공간 유지
+							)}
 						</FormItem>
 					)}
 				/>
