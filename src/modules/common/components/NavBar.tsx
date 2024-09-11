e80b3b704ffca393af1dc63a8fe8b56353fdebaf
이민선 <woooooooow22@gmail.com>
2024-09-11 13:56:17 +0900

    feat: 로그아웃 구현

--- src/modules/common/components/NavBar.tsx
+++ src/modules/common/components/NavBar.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image"
 import Link from "next/link"
+import useAuthStore from "@/modules/auth/store/AuthStore"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export const NavBar = () => {
+	const { isLoggedIn, logout } = useAuthStore()
 	return (
 		<header>
 			<nav className="fixed flex h-16 w-screen flex-row items-center justify-between whitespace-nowrap border border-b-2 border-slate-200">
@@ -16,12 +19,21 @@ export const NavBar = () => {
 				</Link>
 				<Input placeholder="게시물을 검색해보세요." className="w-1/5 min-w-52" />
 				<div className="flex w-1/2 items-center justify-center gap-x-8">
-					<Link href="/auth/signup">
-						<button>회원가입</button>
-					</Link>
-					<Link href="/auth/signin">
-						<button>로그인</button>
-					</Link>
+					{isLoggedIn ? (
+						<>
+							<Image src="/avatar.svg" alt="profile image" width={30} height={30} />님
+							<button onClick={logout}>로그아웃</button>
+						</>
+					) : (
+						<>
+							<Link href="/auth/signup">
+								<button>회원가입</button>
+							</Link>
+							<Link href="/auth/signin">
+								<button>로그인</button>
+							</Link>
+						</>
+					)}
 					<Link href="/chat">
 						<Button variant="outline">채팅하기</Button>
 					</Link>
