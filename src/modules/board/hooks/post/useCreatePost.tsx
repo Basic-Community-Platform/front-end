7686c63c22d2715bc9497cbc31dc3847b2eb5adb
이민선 <woooooooow22@gmail.com>
2024-10-19 07:46:58 +0900

    feat: 글쓰기 토스트 구현

--- src/modules/board/hooks/post/useCreatePost.tsx
+++ src/modules/board/hooks/post/useCreatePost.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import { useMutation } from "@tanstack/react-query"
+import { useToast } from "@/hooks/use-toast"
 import api from "@/modules/auth/api"
 
 interface CreatedPost {
@@ -9,16 +10,16 @@ interface CreatedPost {
 
 const createPost = async (data: CreatedPost) => {
 	const response = await api.post("/api/posts", data)
-	alert(response.statusText)
 	return response
 }
 
 export const useCreatePost = () => {
 	const router = useRouter()
+	const { toast } = useToast()
 	return useMutation({
 		mutationFn: createPost,
 		onSuccess: () => {
-			alert("success")
+			toast({ title: "success" })
 			router.push("/board")
 		},
 		onError: (error) => {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import RootLayout from "./RootLayout"
 import axios from "axios"
+import { Toaster } from "@/components/ui/toaster"
 import "@/styles/globals.css"
 
 const queryClient = new QueryClient()
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
 		<RootLayout>
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
+				<Toaster />
 			</QueryClientProvider>
 		</RootLayout>
 	)
