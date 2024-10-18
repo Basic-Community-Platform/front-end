1c5ef552916d0f735eb8c34356edd3eebdd7b5b4
이민선 <woooooooow22@gmail.com>
2024-10-19 02:14:54 +0900

    feat: 전체 게시물 테이블 생성

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,19 +1,45 @@
 import Link from "next/link"
+import { useFetchAllPosts } from "@/modules/board/hooks/post/useFetchAllPosts"
 import { PencilLine } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 const BoardPage = () => {
+	const { data: posts } = useFetchAllPosts()
+
 	return (
-		<div>
-			<Link href="/board/create">
-				<Button>
-					<PencilLine className="mr-2 h-4 w-4" />
-					글쓰기
-				</Button>
-			</Link>
-			<Link href="/board/2">
-				<Button>2번 게시물</Button>
-			</Link>
+		<div className="w-2/3">
+			<div>
+				<span>총 게시물 {posts?.length || 0}건</span>
+			</div>
+			<Table className="border-t-2 border-slate-600">
+				<TableHeader className="bg-slate-50">
+					<TableRow>
+						<TableHead className="whitespace-nowrap">게시판</TableHead>
+						<TableHead className="w-full">제목</TableHead>
+						<TableHead className="whitespace-nowrap">등록자명</TableHead>
+						<TableHead className="whitespace-nowrap">조회수</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{posts?.toReversed().map((post) => (
+						<TableRow key={post.postId}>
+							<TableCell className="text-center">{post.postId}</TableCell>
+							<TableCell className="font-medium">{post.title}</TableCell>
+							<TableCell className="text-center">{post.loginId}</TableCell>
+							<TableCell className="text-center">{post.viewCount}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+			<div className="flex justify-end py-6">
+				<Link href="/board/create">
+					<Button>
+						<PencilLine className="mr-2 h-4 w-4" />
+						글쓰기
+					</Button>
+				</Link>
+			</div>
 		</div>
 	)
 }
