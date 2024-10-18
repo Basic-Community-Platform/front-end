7d5e5f0d730f4a176c39b286b46a84fd97c2ac67
이민선 <woooooooow22@gmail.com>
2024-10-19 07:20:02 +0900

    feat: 게시물이 존재하지 않을 경우 문구 생성

--- src/modules/board/layout/BoardLayout.tsx
+++ src/modules/board/layout/BoardLayout.tsx
@@ -1,3 +1,3 @@
 export const BoardLayout = ({ children }: { children: React.ReactNode }) => {
-	return <main className="flex w-full flex-col items-center justify-center py-40">{children}</main>
+	return <main className="flex w-screen flex-col items-center justify-center py-40">{children}</main>
 }
