caf92ddaaca6623a74ba893a41a26181207b9735
이민선 <woooooooow22@gmail.com>
2024-10-19 02:50:01 +0900

    style: 테이블 스타일 변경

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -11,24 +11,26 @@ const BoardPage = () => {
 	return (
 		<div className="w-2/3">
 			<div>
-				<span className="flex flex-row py-6">
+				<span className="flex flex-row items-center py-6 text-sm font-semibold">
 					<FileText />총 게시물 {posts?.length || 0}건
 				</span>
 			</div>
 			<Table className="border-t-2 border-slate-600">
 				<TableHeader className="bg-slate-50">
-					<TableRow>
-						<TableHead className="whitespace-nowrap">게시판</TableHead>
-						<TableHead className="w-full">제목</TableHead>
-						<TableHead className="whitespace-nowrap">등록자명</TableHead>
-						<TableHead className="whitespace-nowrap">조회수</TableHead>
+					<TableRow className="whitespace-nowrap">
+						<TableHead className="font-semibold">게시판</TableHead>
+						<TableHead className="w-full font-semibold">제목</TableHead>
+						<TableHead className="font-semibold">등록자명</TableHead>
+						<TableHead className="font-semibold">조회수</TableHead>
 					</TableRow>
 				</TableHeader>
 				<TableBody>
 					{posts?.toReversed().map((post) => (
 						<TableRow key={post.postId}>
 							<TableCell className="text-center">{post.postId}</TableCell>
-							<TableCell className="font-medium">{post.title}</TableCell>
+							<Link href={`board/${post.postId}`} key={post.postId}>
+								<TableCell className="w-screen font-medium">{post.title}</TableCell>
+							</Link>
 							<TableCell className="text-center">{post.loginId}</TableCell>
 							<TableCell className="text-center">{post.viewCount}</TableCell>
 						</TableRow>
